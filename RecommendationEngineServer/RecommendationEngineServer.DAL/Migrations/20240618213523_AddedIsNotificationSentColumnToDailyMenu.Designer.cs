18e0bae8ffb3d110d1b99314bb15d1d5adf0c055
ayush.srivastava <ayush.srivastava@ITT-AYUSH-SRIV>
2024-06-19 03:06:44 +0530

    Added migration for new column IsNotificationSent in DailyMenu table

--- RecommendationEngineServer/RecommendationEngineServer.DAL/Migrations/20240618213523_AddedIsNotificationSentColumnToDailyMenu.Designer.cs
+++ RecommendationEngineServer/RecommendationEngineServer.DAL/Migrations/20240618213523_AddedIsNotificationSentColumnToDailyMenu.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RecommendationEngineServer.DAL;
 
@@ -11,9 +12,10 @@ using RecommendationEngineServer.DAL;
 namespace RecommendationEngineServer.DAL.Migrations
 {
     [DbContext(typeof(RecommendationEngineDBContext))]
-    partial class RecommendationEngineDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240618213523_AddedIsNotificationSentColumnToDailyMenu")]
+    partial class AddedIsNotificationSentColumnToDailyMenu
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
